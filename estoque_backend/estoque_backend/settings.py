e788ad9ea798aee08b7578669a7126bb889c497e
Gigioss <49174006+Gigioss@users.noreply.github.com>
2025-04-12 16:34:51 -0300

    Update settings.py

--- estoque_backend/estoque_backend/settings.py
+++ estoque_backend/estoque_backend/settings.py
@@ -131,8 +131,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'estoque_backend/static'),  # Development files
+    os.path.join(BASE_DIR, 'static'),                 # Additional directory if needed
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
