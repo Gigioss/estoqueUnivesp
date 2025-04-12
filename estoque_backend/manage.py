9c5baed62994945ca1fef93453f79760067ca1a6
Gigioss <49174006+Gigioss@users.noreply.github.com>
2025-04-12 10:21:23 -0300

    Update manage.py

--- estoque_backend/manage.py
+++ estoque_backend/manage.py
@@ -2,12 +2,16 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-sys.path.append(str(BASE_DIR))
-
+from pathlib import Path  # Adicione esta linha
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estoque_backend.estoque_backend.settings')
+    # Adicione estas 3 linhas antes do setdefault:
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    sys.path.append(str(BASE_DIR))
+    sys.path.append(str(BASE_DIR / 'estoque_backend'))
+    
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estoque_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,7 +21,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
