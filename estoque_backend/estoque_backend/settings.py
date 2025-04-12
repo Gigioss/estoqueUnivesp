a62f0cbcc8d7a277f03aa5449ac2662f26f6e5e0
Gigioss <49174006+Gigioss@users.noreply.github.com>
2025-04-12 17:30:50 -0300

    Update settings.py

--- estoque_backend/estoque_backend/settings.py
+++ estoque_backend/estoque_backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=cbs*4nmbzh!4uvvgrjxg&cibjeb%97zk^%5y6d)m!ub0(&g2&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
